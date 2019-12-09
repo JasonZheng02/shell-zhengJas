59d4d52f2f7a644ab884656e4a0250277aa3f434
JasonZheng02 <heydude506@gmail.com>
2019-12-09 15:09:49 -0500

    parse_args

--- main.c
+++ main.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-char * parse_args( char * line ){
-  char * args[5];
+char ** parse_args( char * line ){
   char * curr = line;
+  char ** args = malloc(100);
   int x;
-  for (x=0; curr != NULL; x++){
+  for (x = 0; curr != NULL; x++){
     args[x] = strsep (&curr, " ");
   }
   return args;
 }
+
 int main(int argc, char *argv[]) {
-  char* input = malloc(100);
+  char input[100];
   if (argc > 0){
     fgets(input,100,stdin);
     input[strlen(input)-1] = 0;
   }
+  printf("%s\n", input);
   char ** args = parse_args(input);
-  printf("%s\n", args);
+  execvp(args[0], args);
   return 0;
 }
