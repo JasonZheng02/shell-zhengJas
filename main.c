74b4aa1e4c4a8fb15345760e1a719f9016446ebd
Jason Zheng <jzheng02@stuy.edu>
2019-12-16 20:29:43 -0500

    .

--- main.c
+++ main.c
@@ -93,6 +93,7 @@ int main(){
 		cmds = parse(input, ";");
 		int i;
 		for (i = 0; cmds[i] != NULL; i ++){
+			printf("%s",cmds[i]);
 			cmds[i] = remove_spaces(cmds[i]);
 			args = parse(cmds[i], "-");
 			execArgs(args);
